4ee01f5af3ab67334d839adb975ac0575731c0d1
srinivas-ds <srinivasduggampudi@gmail.com>
2021-07-29 08:21:33 +0530

    branch1 commit

--- GitJavaProject1/src/main/java/com/lti/demo/UserApp.java
+++ GitJavaProject1/src/main/java/com/lti/demo/UserApp.java
@@ -4,5 +4,6 @@ public class UserApp {
 
 public static void main(String[] args) {
 	System.out.println("this is of my own practise");
+	System.out.println("i am in branch1 !! i am working ont it and merge this to main after completion ");
 }
 }
