a7d59853120b043d952d8b795d480f8e82e4dffe
srinivas-ds <srinivasduggampudi@gmail.com>
2021-07-29 08:30:08 +0530

    newthing in branch1

--- GitJavaProject1/src/main/java/com/lti/demo/UserApp.java
+++ GitJavaProject1/src/main/java/com/lti/demo/UserApp.java
@@ -3,7 +3,7 @@ package com.lti.demo;
 public class UserApp {
 
 public static void main(String[] args) {
-	System.out.println("this is of my own practise");
+	
 	System.out.println("i am in branch1 !! i am working ont it and merge this to main after completion ");
 }
 }
